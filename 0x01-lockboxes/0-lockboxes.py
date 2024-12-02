03117e641aa1a82d0420d8d5b782988daa0b4f19
AtlasLioness <narjisaliane@gmail.com>
2024-12-02 18:17:58 +0000

    0. Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,14 +8,16 @@ def canUnlockAll(boxes):
     """
     Method to check if all boxes can be opened
     """
-    unlocked = {0}
+    n = len(boxes)
+    visited = set()
     queue = [0]
+    visited.add(0)
 
     while queue:
-        box = queue.pop()
+        current = queue.pop(0)
 
-        for key in boxes[box]:
-            if key not in unlocked:
-                unlocked.add(key)
+        for key in boxes[current]:
+            if key not in visited and 0 <= key < n:
+                visited.add(key)
                 queue.append(key)
-    return len(unlocked) == len(boxes)
+    return len(visited) == n
